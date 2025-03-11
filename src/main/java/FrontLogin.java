345f8d0e8d05c85d53ea1f3f335afdae67831a1f
amanda.santos <amandalarasantos0914@gmail.com>
2025-03-11 11:22:52 -0300

    Mudanças na configuração do gradle.properties

---
+++ src/main/java/FrontLogin.java
@@ -0,0 +1,13 @@
+public class FrontLogin {
+    public static void main(String[] args) {
+
+        System.out.println("╔════════════════════════════════════╗");
+        System.out.println("║               Login                ║");
+        System.out.println("╚════════════════════════════════════╝");
+        System.out.println();
+
+        System.out.println("Digite o seu CPF ou CNPJ:");
+
+        System.out.println("Digite sua senha:");
+    }
+}
